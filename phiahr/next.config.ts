faa091cef3466cc7e948ec5ff2edbfa3af738e99
Philipp Ahrendt <pipo.ahrendt@gmail.com>
2025-02-10 14:08:23 +0100

    Build static page

--- phiahr/next.config.ts
+++ phiahr/next.config.ts
@@ -6,7 +6,9 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,  // Ensures images work during export
   },
-  assetPrefix: '/'
+  assetPrefix: '/phiahr',
+  trailingSlash: true, // Adds a slash to the end of every route for compatibility
+
 };
 module.exports = nextConfig
 // export default nextConfig;
